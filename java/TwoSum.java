174fa59a9bf2b97fe1b02bc500677672b299891e
Luis Mateo Arias Caicedo <mateo.arias@jala.university>
2024-03-18 17:19:02 -0500

    ...

---
+++ java/TwoSum.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.HashMap;
+
+class Solution {
+
+  public int[] twoSum(int[] nums, int target) {
+    var previousNumbers = new HashMap<Integer, Integer>();
+    int[] output = new int[2];
+
+    for (int i = 0; i < nums.length; i++) {
+      int difference = target - nums[i];
+      Integer differenceIndex = previousNumbers.get(difference);
+
+      if (differenceIndex != null) {
+        output[0] = i;
+        output[1] = differenceIndex;
+      }
+
+      previousNumbers.put(nums[i], i);
+    }
+
+    return output;
+  }
+}
